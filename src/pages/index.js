5a73521eb9e921b6b11c11290532b95e6136ed77
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-07-21 19:24:34 +0530

    folder structure set up

---
+++ src/pages/index.js
@@ -0,0 +1,47 @@
+// import React, { Component } from "react";
+// import Nav from "../components/Navigation";
+// import Portfolio from "./Portfolio";
+// import Resume from "./Resume";
+// import AboutMe from "./AboutMe";
+
+// // import Footer from "../components/Footer";
+// // import Header from "../components/Header";
+
+// //all basic components here
+
+// //Header, Footer, etc
+
+// class MainPage extends Component {
+//   render() {
+//     return (
+//       <>
+//         {/* <Header />
+//         <Footer /> */}
+//       </>
+//     );
+//   }
+// }
+
+// export default MainPage;
+
+// import React, { Component } from "react";
+
+// // import Footer from "../components/Footer";
+// // import Header from "../components/Header";
+
+// //all basic components here
+
+// //Header, Footer, etc
+
+// class MainPage extends Component {
+//   render() {
+//     return (
+//       <>
+//         {/* <Header />
+//         <Footer /> */}
+//       </>
+//     );
+//   }
+// }
+
+// export default MainPage;
