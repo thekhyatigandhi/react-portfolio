a190eee2fb77db33f5d06bf3424ccdd90d22d4f0
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-07-25 14:10:30 -0400

    helpers added to utils

---
+++ src/utils/helpers.js
@@ -0,0 +1,13 @@
+export function validateEmail(email) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+export function validateUserName(input) {
+  return input.length > 0;
+}
+
+export function validateMessage(input) {
+  return input.length > 0;
+}
